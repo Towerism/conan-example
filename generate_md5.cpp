0b7164b93fd3db95cc2b6a5cfae7f560542aca87
6367f766 <6367f766@users.noreply.github.com>
2022-12-18 22:30:13 +0000

    Format code according to .clang-format
    Signed-off-by: 6367f766 <6367f766@users.noreply.github.com>

--- generate_md5.cpp
+++ generate_md5.cpp
@@ -1,14 +1,14 @@
-#include "Poco/MD5Engine.h"
 #include "Poco/DigestStream.h"
+#include "Poco/MD5Engine.h"
 
 #include <iostream>
 #include <string>
 
 std::string generate_md5()
 {
-  Poco::MD5Engine md5;
-  Poco::DigestOutputStream ds(md5);
-  ds << "abcdefghijklmnopqrstuvwxyz";
-  ds.close();
-  return Poco::DigestEngine::digestToHex(md5.digest());
+    Poco::MD5Engine md5;
+    Poco::DigestOutputStream ds(md5);
+    ds << "abcdefghijklmnopqrstuvwxyz";
+    ds.close();
+    return Poco::DigestEngine::digestToHex(md5.digest());
 }
--- md5.cpp
+++ md5.cpp
@@ -1,11 +1,10 @@
 #include "generate_md5.hh"
-
 #include <iostream>
-
 
 int main(int argc, char** argv)
 {
-  const auto md5 = generate_md5();
-  std::cout << md5 << std::endl;
-  return 0;
+    int a = 5;
+    const auto md5 = generate_md5();
+    std::cout << md5 << std::endl;
+    return 0;
 }
